a1a1eea10d291bf00f6c65859c69a89ac43cee2c
Till Knollmann <till-knollmann@outlook.de>
2023-03-28 17:28:07 +0000

    Improve font handling

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+ window.addEventListener("load", () => {
+    function sendData() {
+      const XHR = new XMLHttpRequest();
+
+      // Bind the FormData object and the form element
+      const FD = new FormData(form);
+
+      // Define what happens on successful data submission
+      XHR.addEventListener("load", (event) => {
+        let button = document.getElementById("submit-button");
+        button.innerHTML = "Message sent!";
+        button.setAttribute("disabled", "");
+      });
+
+      // Define what happens in case of error
+      XHR.addEventListener("error", (event) => {
+        console.error('Request to server failed');
+      });
+
+      // Set up our request
+      XHR.open("POST", "submit.php");
+
+      // The data sent is what the user provided in the form
+      XHR.send(FD);
+    }
+
+    // Get the form element
+    const form = document.getElementById("contact-form");
+
+    // Add 'submit' event handler
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let button = document.getElementById("submit-button");
+        button.innerHTML = "Sending...";
+        button.setAttribute("disabled", "");
+
+      sendData();
+    });
+  });

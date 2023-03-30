67675069355304bbfd6355168e57f9231b744270
Till Knollmann <till-knollmann@outlook.de>
2023-03-30 13:29:58 +0000

    add generator script for multi-language page

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,24 @@
       // Bind the FormData object and the form element
       const FD = new FormData(form);
 
+      // determine language
+      var notifications;
+
+      if (lang === "de") {
+        notifications = de;
+      }
+      if (lang === "en") {
+        notifications = en;
+      }
+
+      let button = document.getElementById("submit-button");
+      button.innerHTML = notifications[0];
+      button.setAttribute("disabled", "");
+
       // Define what happens on successful data submission
       XHR.addEventListener("load", (event) => {
         let button = document.getElementById("submit-button");
-        button.innerHTML = "Message sent!";
+        button.innerHTML = notifications[1];
         button.setAttribute("disabled", "");
         document.getElementById("name").setAttribute("disabled", "");
         document.getElementById("email").setAttribute("disabled", "");
@@ -30,13 +44,15 @@
     // Get the form element
     const form = document.getElementById("contact-form");
 
+    // get the language
+    const lang = document.getElementsByTagName("html")[0].getAttribute("lang");
+
+    const en = ["Sending ...", "Sent!"];
+    const de = ["Sendet ...", "Gesendet!"];
+
     // Add 'submit' event handler
     form.addEventListener("submit", (event) => {
       event.preventDefault();
-      let button = document.getElementById("submit-button");
-        button.innerHTML = "Sending...";
-        button.setAttribute("disabled", "");
-
       sendData();
     });
   });

1378d74c641c20923c7c25d51db05fdc254da38a
Till Knollmann <till-knollmann@outlook.de>
2023-03-31 15:05:15 +0000

    Improve publication list layout and style

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-$acceptLang = ['en'];
+$acceptLang = ['de', 'en'];
 $lang = in_array($lang, $acceptLang) ? $lang : 'en';
 header("Location: " . "/" . $lang . "/index.html", true, 303);
 exit();

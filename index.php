acae889a1820f727d67145009a4fc1eba8dbb6e5
Till Knollmann <till-knollmann@outlook.de>
2023-04-03 14:22:16 +0000

    improve redirect

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 $acceptLang = ['de', 'en'];
 $lang = in_array($lang, $acceptLang) ? $lang : 'en';
-header("Location: " . "/" . $lang . "/index.html", true, 303);
+header("Location: " . "/" . $lang . "/", true, 303);
 exit();

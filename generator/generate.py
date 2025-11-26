d026ec19ea785985ac18d77c2d9cb97407de69b0
Till Knollmann <till-knollmann@outlook.de>
2025-11-26 23:29:44 +0000

    🎨 formatted python code

--- generator/generate.py
+++ generator/generate.py
@@ -4,6 +4,7 @@ from projects_generator import generate_projects_html
 
 tag_hotwords = ["$about-text$", "$contact-text$"]
 
+
 def generate_page(lang: str, path: str, cwd: str):
     ''' Generates the webpage using the file "template.html"
     in cwd, applying the language of lang using the replacements
@@ -23,17 +24,19 @@ def generate_page(lang: str, path: str, cwd: str):
                 for hotword in lang_code.keys():
 
                     to_plug_in = lang_code[hotword]
-                    
+
                     # Special handling for projects dictionary
                     if hotword == "$projects$":
                         to_plug_in = generate_projects_html(to_plug_in, cwd)
-                        page_content = page_content.replace("$projects-content$", to_plug_in)
+                        page_content = page_content.replace(
+                            "$projects-content$", to_plug_in)
                         continue
 
                     if hotword in tag_hotwords:
                         # tag text to be plugged in
                         for tag in tags_content.keys():
-                            to_plug_in = to_plug_in.replace(tag, "<a target=\"_blank\" href=\"" + tags_content[tag] + "\">" + tag + "</a>")
+                            to_plug_in = to_plug_in.replace(
+                                tag, "<a target=\"_blank\" href=\"" + tags_content[tag] + "\">" + tag + "</a>")
 
                     page_content = page_content.replace(hotword, to_plug_in)
 
@@ -51,5 +54,4 @@ languages = ["de", "en"]
 paths = ["/".join(cwd.split("/")[:-1] + [lang]) for lang in languages]
 
 for i in range(2):
-    generate_page(languages[i], paths[i], cwd )
-
+    generate_page(languages[i], paths[i], cwd)
